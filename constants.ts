0b7071c889abe7520e6012b9a5fc9606f75dbac7
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2022-02-19 14:26:25 -0300

    Fix SITE_URL

--- constants.ts
+++ constants.ts
@@ -1,3 +1,4 @@
-export const SITE_URL = process.env.VERCEL_URL ?? "http://localhost:3000";
+export const SITE_URL =
+  process.env.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000";
 export const SEO_URL = "https://dashboardify.app";
 export const GITHUB_URL = "https://github.com/BrunoQuaresma/dashboardify";
