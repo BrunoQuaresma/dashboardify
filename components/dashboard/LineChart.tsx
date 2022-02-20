fdf6a299120be45484e1c35cbbfe5e6dbfa9365f
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2022-02-20 18:02:02 -0300

    Fix responsive for chart

--- components/dashboard/LineChart.tsx
+++ components/dashboard/LineChart.tsx
@@ -1,29 +1,46 @@
 import React from "react";
 import { LineChart as LineChartType } from "../../types";
 import { ResponsiveLine } from "@nivo/line";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, useBreakpointValue } from "@chakra-ui/react";
 
 export const LineChart: React.FC<{ chart: LineChartType }> = ({ chart }) => {
+  const isSimple = useBreakpointValue({ base: true, md: false });
+
   return (
-    <Box shadow="sm" p={[4, 6]} rounded="md" bg="white" w="full" height="md">
-      <Heading as="h4" fontSize="sm" mb={4} fontWeight="medium">
+    <Flex
+      shadow="sm"
+      rounded="md"
+      bg="white"
+      w="full"
+      height={{ base: "xs", md: "md" }}
+      flexDir="column"
+    >
+      <Heading as="h4" fontSize="sm" fontWeight="medium" p={[4, 6]}>
         {chart.label}
       </Heading>
 
-      <ResponsiveLine
-        enableSlices="x"
-        animate
-        colors={chart.lines.map((l) => l.color)}
-        data={chart.lines}
-        margin={{ top: 10, right: 110, bottom: 60, left: 60 }}
-        yScale={{
-          type: "linear",
-          min: "auto",
-          max: "auto",
-          stacked: true,
-          reverse: false,
-        }}
-      />
-    </Box>
+      <Box p={{ base: 0, md: 6 }} pt={0} w="full" flex="1">
+        <ResponsiveLine
+          animate
+          enablePoints={!isSimple}
+          enableSlices="x"
+          colors={chart.lines.map((l) => l.color)}
+          data={chart.lines}
+          margin={
+            isSimple
+              ? { top: 0, right: 0, bottom: 0, left: 0 }
+              : { top: 10, right: 40, bottom: 40, left: 40 }
+          }
+          yScale={{
+            type: "linear",
+            max: "auto",
+          }}
+          enableGridX={!isSimple}
+          enableGridY={!isSimple}
+          axisBottom={isSimple ? null : undefined}
+          axisLeft={isSimple ? null : undefined}
+        />
+      </Box>
+    </Flex>
   );
 };
