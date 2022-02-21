5ea8d554014c6fccb0aa1ae5f6c7b4fcace34d37
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2022-02-21 13:08:00 -0300

    Fix canonical

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 export const SITE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : "http://localhost:3000";
-export const SEO_URL = "https://dashboardify.app";
+export const SEO_URL = "https://www.dashboardify.app";
 export const GITHUB_URL = "https://github.com/BrunoQuaresma/dashboardify";
